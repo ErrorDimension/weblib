28f6411a40d58edb9e8555ca741134c494ee6813
ErrorDIM <mrxiaolin2005@gmail.com>
2022-05-10 15:45:37 +0700

    🥲 `damn` : web api sucks

--- js/stopclock.js
+++ js/stopclock.js
@@ -1,7 +1,12 @@
+const windowIsDefined = typeof window !== 'undefined';
 class StopClock {
     startPoint;
-    get click() { return (performance.now() - this.startPoint) / 1000; }
-    constructor(startPoint = performance.now()) {
+    get click() {
+        return windowIsDefined
+            ? (window.performance.now() - this.startPoint) / 1000
+            : -1;
+    }
+    constructor(startPoint = windowIsDefined ? window.performance.now() : -1) {
         this.startPoint = startPoint;
     }
 }
--- ts/stopclock.ts
+++ ts/stopclock.ts
@@ -1,9 +1,16 @@
+const windowIsDefined = typeof window !== 'undefined'
+
+
 class StopClock {
     private startPoint: number
 
-    public get click(): number { return (performance.now() - this.startPoint) / 1000 }
+    public get click(): number {
+        return windowIsDefined
+            ? (window.performance.now() - this.startPoint) / 1000
+            : -1
+    }
 
-    constructor(startPoint: number = performance.now()) {
+    constructor(startPoint: number = windowIsDefined ? window.performance.now() : -1) {
         this.startPoint = startPoint
     }
 }
