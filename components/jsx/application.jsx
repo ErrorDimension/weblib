3b6af99d5fcc659f74f1b1d1068512a7c50ebbe2
ErrorDIM <mrxiaolin2005@gmail.com>
2022-05-15 14:26:24 +0700

    😅 `incoming`

--- components/jsx/application.jsx
+++ components/jsx/application.jsx
@@ -1,54 +1,68 @@
 import { Component } from 'react'
+import Link from 'next/link'
+
+import magicDOM from './../../js/magic-dom'
+import { $$ } from './../../js/jquery'
 
 
-export default class App extends Component {
-    constructor(props) {
-        super(props)
-    }
-
-
-    render() {
-        const { children } = this.props
-
-        return (<>{children}</>)
-    }
+export default function Application() {
+    return <></>
 }
 
 
 export class Nav extends Component {
-    constructor(props) {
-        super(props)
+    constructor(props) { super(props) }
+
+
+    componentDidMount() {
+        const { query } = this.props
+
+
+        $$(query).appendChild(magicDOM.createElement('style', {
+            children: `
+            ${query} {
+                display: flex;
+                flex-direction: column;
+                place-items: center;
+                
+                height: 100vh;
+            }
+
+            ${query} > ${query}-content {
+                height: 100%;
+                width: 1200px;
+            }
+        `}))
     }
 
 
     render() {
         const { children } = this.props
 
-        return (
-            <nav className="nav">
-                {children}
-            </nav>
-        )
+        return <nav className='nav'>{children}</nav>
+    }
+}
+
+
+export class NavLink extends Component {
+    constructor(props) { super(props) }
+
+
+    render() {
+        const { href, icon } = this.props
+
+        return <Link href={href}><a>{icon}</a></Link>
     }
 }
 
 
 export class Setting extends Component {
-    constructor(props) {
-        super(props)
-    }
-
-
-    componentDidMount() { }
+    constructor(props) { super(props) }
 
 
     render() {
         const { children } = this.props
 
-        return (
-            <div className="setting">
-                {children}
-            </div>
-        )
+        return <div className='setting'>{children}</div>
     }
 }
